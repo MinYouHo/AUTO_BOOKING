b322a853186d4fca1822e82e0b5db61ff26dd918
MinYouHo <b404075410@gmail.com>
2024-05-07 00:43:29 +0800

    整理需手動調整的變數

--- request_booking.py
+++ request_booking.py
@@ -1,11 +1,20 @@
 import requests, time
-from bs4 import BeautifulSoup
 
+# GA cookies
+ga = ''
+ga_L47EP67E8W = ''
+gid = ''
+
+# 想要的琴房
+room = '2C'
+# 想要的時段：s + 第幾時段
+section = ['s14', 's15']
+
+# 學校帳號
 account = {'muid': '',
-          'mpassword': '',
-          'forceMobile': 'pc'}
-""" # YYYYMMDD
-roomIwant =  """
+           'mpassword': '',
+           'forceMobile': ''}
+
 headers = {
     'Host': 'iportal.ntnu.edu.tw',
     'Referer': 'https://iportal.ntnu.edu.tw/ntnu/',
@@ -13,86 +22,70 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0'
 }
 cookies = ''
-domain = 'https://iportal.ntnu.edu.tw/'
-url = domain + 'login.do'
+t = time.localtime(time.time())
 
 fp = open("log.txt", "a", encoding='utf8')
+fp.write(time.strftime('%Y/%m/%d %H:%M:%S\n',t))
 
 """ 登入 """
+domain = 'https://iportal.ntnu.edu.tw/'
+url = domain + 'login.do'
 r = requests.post(url, headers=headers, data=account)
 if r.status_code != requests.codes.ok:
-    print("Login Failse:", r.status_code)
-    print(r.raise_for_status())
     fp.write("Login Failse:", r.status_code)
     fp.write(r.raise_for_status())
     fp.close()
     exit()
-print("Login Success")
 fp.write("Login Success: " + str(r.status_code) + "\n")
 cookies = r.cookies.get_dict()
-print(str(cookies))
 
 """ 進校網 """
 # time.sleep(1)
 headers['Referer'] = url
 # url = domain + 'myPortal.do'
 index = r.text.find('myPortal.do?thetime=')
-t = r.text[index+20:index+33]
 url = domain + r.text[index:index+33] # params = {'thetime': t}
 r = requests.get(url, headers=headers, cookies=cookies)
 if r.status_code != requests.codes.ok:
-    print("Enter School Web Failse:", r.status_code)
-    print(r.raise_for_status())
     fp.write("Enter School Web Failse:", r.status_code + '\n')
     fp.write(r.raise_for_status())
     fp.close()
     exit()
-print("Enter School Web Success")
 fp.write("Enter School Web Success: " + str(r.status_code) + "\n")
-print(str(cookies))
 
-""" 進琴房 (1/2) """ # 需要JSESSIONID
+# 進琴房 (1/2)， 需要JSESSIONID
 headers['Sec-Fetch-Site'] = 'none'
-url = domain + 'ssoIndex.do?apUrl=https://pms.itc.ntnu.edu.tw/BookMeApp/BookMe1010Ctrl?action=doLogin&apOu=Practice_Piano_Room_Booking&sso=true&datetime1='+t
+datetime = r.text[index+20:index+33]
+url = domain + 'ssoIndex.do?apUrl=https://pms.itc.ntnu.edu.tw/BookMeApp/BookMe1010Ctrl?action=doLogin&apOu=Practice_Piano_Room_Booking&sso=true&datetime1='+datetime
 r = requests.get(url, cookies=cookies)
 if r.status_code != requests.codes.ok:
-    print("Enter Booking Web (1/2) Failse:", r.status_code)
-    print(r.raise_for_status())
     fp.write("Enter Booking Web (1/2) Failse:", r.status_code + '\n')
     fp.write(r.raise_for_status())
     fp.close()
     exit()
-print("Enter Booking Web (1/2) Success")
 fp.write("Enter Booking Web (1/2) Success: " + str(r.status_code) + "\n")
-print(str(cookies))
-
-""" 進琴房 (2/2) """ # 會給新的JSESSIONID
-domain = 'https://pms.itc.ntnu.edu.tw/'
-url = domain + 'BookMeApp/BookMe1010Ctrl?action=doLogin'
-# 取得data
-index1 = r.text.find("sessionId' value='")
-index2 = r.text.find("'>\n<input type='hidden' name='userid")
-sessionId = r.text[index1+18:index2]
+# get data
+sessionId = r.text[r.text.find("sessionId' value='")+18:r.text.find("'>\n<input type='hidden' name='userid")]
 userid = account['muid']
 data = {'sessionId': sessionId, 'userid': userid}
+
+# 進琴房 (2/2)， 會給新的JSESSIONID
+domain = 'https://pms.itc.ntnu.edu.tw/'
+url = domain + 'BookMeApp/BookMe1010Ctrl?action=doLogin'
+
 r = requests.post(url, headers=headers, data=data, cookies=cookies)
 if r.status_code != requests.codes.ok:
-    print("Enter Booking Web (2/2) Failse:", r.status_code)
-    print(r.raise_for_status())
     fp.write("Enter Booking Web (2/2) Failse:", r.status_code + '\n')
     fp.write(r.raise_for_status())
     fp.close()
     exit()
-print("Enter Booking Web (2/2) Success")
 fp.write("Enter Booking Web (2/2) Success: " + str(r.status_code) + "\n")
-cookies = r.cookies
+cookies = r.cookies.get_dict()
+cookies['_ga'] = ga
+cookies['_gid'] = gid
+cookies['_ga_L47EP67E8W'] = ga_L47EP67E8W
 
-# Google Analytics cookies需要手動更新
-cookies['_ga'] = ''
-cookies['_gid'] = ''
-cookies['_ga_L47EP67E8W'] = ''
-
-""" 預約琴房 """
+# 預約琴房
 headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
 headers['Host'] = 'pms.itc.ntnu.edu.tw'
 headers['Origin'] = 'https://pms.itc.ntnu.edu.tw'
@@ -101,13 +94,13 @@ headers['Sec-Fetch-Dest'] = 'empty'
 headers['Sec-Fetch-Mode'] = 'cors'
 headers['Sec-Fetch-Site'] = 'same-origin'
 headers['X-Requested-With'] = 'XMLHttpRequest'
-# data = {'action': 'doBookMe', 'data': '202404303Ds05'} # (年月日 + 房號) + 時段
-data = {'action': 'doGoTo1050'} # 琴房狀態
-# data = {'action': 'doBookMe', 'data': 's05'} # (年月日 + 房號) + 時段
 
-url = domain + 'BookMeApp/BookMe1010Ctrl'
-# url = domain + 'BookMeApp/BookMe1050Ctrl'
-r = requests.post(url, headers=headers, data=data, cookies=cookies)
-print(r.text)
+url = domain + 'BookMeApp/BookMe1050Ctrl'
+data = {'action': 'doBookMe', 'data': ''}
+date = time.strftime('%Y%m%d',t)
+for i in section:
+    data['data'] = date + room + i
+    r = requests.post(url, headers=headers, data=data, cookies=cookies)
+    fp.write(r.text)
 
 fp.close()
